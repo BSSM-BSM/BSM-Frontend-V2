5a442a6543716b8bae2305a5eb675c4fa038c103
leehj050211 <leehj050211@gmail.com>
2022-09-10 22:17:09 +0900

    FIX: 급식 정보 중복 로딩 버그 & 급식 디자인

--- pages/meal.tsx
+++ pages/meal.tsx
@@ -8,9 +8,9 @@ import styles from '../styles/meal.module.css';
 import { dateToShortStr, shrotStrToDate } from "../utils/util";
 
 enum MealTime {
-    MORNING = 'morning',
-    LUNCH = 'lunch',
-    DINNER = 'dinner'
+    MORNING = '아침',
+    LUNCH = '점심',
+    DINNER = '저녁'
 }
 
 interface MealType {
@@ -32,132 +32,121 @@ const MealPage: NextPage = () => {
     const [isSameDay, setIsSameDay] = useState<boolean>(false);
     const [screenScale] = useRecoilState(screenScaleState);
 
-    // inti
+    // init
     useEffect(() => {
-        loadMealList(dateToShortStr(new Date), 0);
-    }, []);
-
-    useEffect(() => {
-        renderMeal();
-    }, [mealList, mealIdx, viewRange]);
-
-    useEffect(() => {
+        (async () => {
+            setMealList(await loadMealList(dateToShortStr(new Date)));
+        })();
         window.addEventListener('resize', calcViewRange);
         return () => {
             window.removeEventListener('resize', calcViewRange);
         }
     }, []);
 
+    useEffect(() => calcViewRange(), [screenScale])
+
     useEffect(() => {
-        calcViewRange();
-    }, [screenScale]);
+        renderMeal();
+    }, [mealList, mealIdx, viewRange]);
 
-    const loadMealList = (date: string, position: number) => {
-        ajax<MealRes>({
-            url: `meal/${date}`,
-            method: HttpMethod.GET,
-            callback(data) {
-                const tempMealList: MealType[] = [];
-                Object.entries(data).forEach(value => {
-                    if (!value[1]) return;
-                    tempMealList.push({
-                        date,
-                        time: Object.values(MealTime)[
-                            Object.keys(MealTime).indexOf(value[0].toUpperCase())
-                        ],
-                        content: value[1]
-                    })
-                });
-
-                switch (position) {
-                    case -1: {
-                        setMealList(prev => [...tempMealList, ...prev]);
-                        setMealIdx(prev => prev + tempMealList.length);
-                        break;
+    const loadMealList = (date: string): Promise<MealType[]> => {
+        return new Promise((resolve, reject) => {
+            ajax<MealRes>({
+                url: `meal/${date}`,
+                method: HttpMethod.GET,
+                callback(data) {
+                    const tempMealList: MealType[] = [];
+                    Object.entries(data).forEach(value => {
+                        if (!value[1]) return;
+                        tempMealList.push({
+                            date,
+                            time: Object.values(MealTime)[
+                                Object.keys(MealTime).indexOf(value[0].toUpperCase())
+                            ],
+                            content: value[1]
+                        });
+                    });
+                    resolve(tempMealList);
+                },
+                errorCallback(data) {
+                    if (data && data.statusCode === 404) {
+                        resolve([{
+                            date,
+                            content: '급식이 없습니다'
+                        }]);
+                        return true;
                     }
-                    case 0: {
-                        setMealList(tempMealList);
-                        break;
-                    }
-                    case 1: {
-                        setMealList(prev => [...prev, ...tempMealList]);
-                        break;
-                    }
-                }
-            },
-            errorCallback(data) {
-                if (data && data.statusCode === 404) {
-                    const emptyMeal = {
-                        date,
-                        content: '급식이 없습니다'
-                    }
-                    switch (position) {
-                        case -1: {
-                            setMealList(prev => [emptyMeal, ...prev]);
-                            setMealIdx(prev => ++prev);
-                            break;
-                        }
-                        case 0: {
-                            setMealList([emptyMeal]);
-                            break;
-                        }
-                        case 1: {
-                            setMealList(prev => [...prev, emptyMeal]);
-                            break;
-                        }
-                    }
-                    return true;
-                }
-            },
+                    reject();
+                },
+            });
         })
     }
 
     const calcViewRange = () => {
         const screenWidth = (window.innerWidth / screenScale) * 100;
-        let range = Math.floor(screenWidth / 450);
+        let range = Math.floor(screenWidth / 250);
         if (range % 2 === 0) range++;
         setViewRange(range);
     }
 
-    const renderMeal = () => {
+    const renderMeal = async () => {
         if (!mealList.length) return;
         if (viewRange % 2 == 0) throw new Error('Even range is not available');
 
         const offset = Math.floor(viewRange / 2);
 
-        const tempMealList: MealType[] = [];
-        let mealLoadFlag = false;
+        const loadPromises: {
+            prev?: Promise<MealType[]>,
+            next?: Promise<MealType[]>
+        } = {};
+        let loadFlag = false;
         
+        const tempMealList: MealType[] = [];
         // 렌더링될 급식 리스트 가져오기
         [...Array(viewRange).keys()].forEach(i => {
-            if (mealList[mealIdx - offset + i])
+            if (mealList[mealIdx - offset + i]) {
                 tempMealList.push(mealList[mealIdx - offset + i]);
+            }
             else {
-                mealLoadFlag = true;
+                loadFlag = true;
                 // 이전, 다음 급식 불러오기
                 if (offset > i) {
+                    if (loadPromises.prev) return;
                     const nextDate = mealList[mealIdx - offset + i + 1]?.date;
                     if (!nextDate) return;
+
                     const prevDate = shrotStrToDate(nextDate);
-                    console.log(prevDate, shrotStrToDate(nextDate).getDate() - 1)
                     prevDate.setDate(
                         shrotStrToDate(nextDate).getDate() - 1
                     );
                     
-                    loadMealList(dateToShortStr(prevDate), -1);
-                } else if (offset < i) {
+                    loadPromises.prev = loadMealList(dateToShortStr(prevDate));
+                } else {
+                    if (loadPromises.next) return;
                     const prevDate = mealList[mealIdx - offset + i - 1]?.date;
                     if (!prevDate) return;
+
                     const nextDate = shrotStrToDate(prevDate);
                     nextDate.setDate(
                         shrotStrToDate(prevDate).getDate() + 1
                     );
 
-                    loadMealList(dateToShortStr(nextDate), 1);
+                    loadPromises.next = loadMealList(dateToShortStr(nextDate));
                 }
             }
         });
-        if (mealLoadFlag) return;
+        if (loadFlag) {
+            const [prevMeals, nextMeals] = await Promise.all([
+                loadPromises.prev?? [],
+                loadPromises.next?? []
+            ]);
+
+            setMealList(prev => [...prevMeals, ...prev, ...nextMeals]);
+            if (prevMeals.length) {
+                setMealIdx(prev => prev + prevMeals.length);
+            }
+            return;
+        };
 
         // 전부 같은 날인지 확인
         tempMealList.some((meal, i) => {
@@ -206,43 +195,44 @@ const MealPage: NextPage = () => {
                 <div className="title center">
                     <h1>급식</h1>
                 </div>
-                <ul className={styles.meals}>
-                    {
-                        renderMealList.map((meal, i) => {
-                            const middleIdx = Math.floor((renderMealList.length) / 2);
-                            return (
-                                <li 
-                                    key={`${meal.date}${meal.time}`}
-                                    onClick={() => {
-                                        if (middleIdx > i) {
-                                            setMealIdx(prev => prev - ((middleIdx) - i));
-                                        } else if (middleIdx < i) {
-                                            setMealIdx(prev => prev - ((middleIdx) - i));
-                                        }
-                                    }}
-                                    className={`${middleIdx === i? styles.center: ''} ${styles[`date_color_${dateColor(meal.date)}`]}`}
-                                >
-                                    {
-                                        isSameDay
-                                        ? middleIdx === i && <h3>
-                                            {meal.date}
-                                        </h3>
-                                        : (
-                                            checkShowMealDate(meal, i) &&
-                                            <h3>
-                                                {meal.date}
-                                            </h3>
-                                        )
-                                    }
-                                    <div className={styles.content}>
-                                        {meal.time}
-                                        {meal.content}
-                                    </div>
-                                </li>
-                            )
-                        })
-                    }
-                </ul>
+                <div className={styles.meals_wrap}>
+                    <ul className={styles.meals}>
+                        {
+                            renderMealList.map((meal, i) => {
+                                const middleIdx = Math.floor((renderMealList.length) / 2);
+                                return (
+                                    <li 
+                                        key={`${meal.date}${meal.time}`}
+                                        onClick={() => {
+                                            if (middleIdx > i) {
+                                                setMealIdx(prev => prev - ((middleIdx) - i));
+                                            } else if (middleIdx < i) {
+                                                setMealIdx(prev => prev - ((middleIdx) - i));
+                                            }
+                                        }}
+                                        className={`${middleIdx === i? styles.center: ''} ${styles[`date_color_${dateColor(meal.date)}`]}`}
+                                    >
+                                        <div>
+                                            {(
+                                                (isSameDay && middleIdx === i) || (!isSameDay && checkShowMealDate(meal, i))
+                                            ) && (
+                                                <h3 className={styles.date}>
+                                                    {meal.date}
+                                                </h3>
+                                            )}
+                                            <h3>{meal.time}</h3>
+                                            <div className={styles.content}>
+                                                {
+                                                    meal.content.split('  ').map(content => <p key={content}>{content}</p>)
+                                                }
+                                            </div>
+                                        </div>
+                                    </li>
+                                )
+                            })
+                        }
+                    </ul>
+                </div>
             </div>
         </div>
     );
