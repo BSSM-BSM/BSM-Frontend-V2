7a3b181a5c054cedad4faeec1cf7a86f56bbc84e
leehj050211 <leehj050211@gmail.com>
2022-10-12 22:27:19 +0900

    ADD: PWA 지원

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,8 +8,17 @@ import Alert from '../components/common/overlay/alert'
 import { LoginBox } from '../components/common/accountPopup'
 import { Header } from '../components/common/header'
 import { SettingBox } from '../components/common/settingPopup'
+import { useEffect } from 'react'
 
 function MyApp({ Component, pageProps }: AppProps) {
+    useEffect(() => {
+        if ("serviceWorker" in navigator) {
+            window.addEventListener("load", () => {
+                navigator.serviceWorker.register("/sw.js");
+            });
+        }
+    }, []);
+
     return (
         <>
             <RecoilRoot>
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,7 +5,21 @@ export default class MyDocument extends Document {
     render() {
         return (
             <Html>
-                <Head />
+                <Head>
+                    <link rel="manifest" href="/manifest.json" />
+                    <meta name="msapplication-TileColor" content="#202124"></meta>
+                    <link rel="apple-touch-icon" href="/icons/app/icon-192x192.png"></link>
+                    <link href="/icons/app/splashscreens/iphone5_splash.png" media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image" />
+                    <link href="/icons/app/splashscreens/iphone6_splash.png" media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image" />
+                    <link href="/icons/app/splashscreens/iphoneplus_splash.png" media="(device-width: 621px) and (device-height: 1104px) and (-webkit-device-pixel-ratio: 3)" rel="apple-touch-startup-image" />
+                    <link href="/icons/app/splashscreens/iphonex_splash.png" media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)" rel="apple-touch-startup-image" />
+                    <link href="/icons/app/splashscreens/iphonexr_splash.png" media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image" />
+                    <link href="/icons/app/splashscreens/iphonexsmax_splash.png" media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3)" rel="apple-touch-startup-image" />
+                    <link href="/icons/app/splashscreens/ipad_splash.png" media="(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image" />
+                    <link href="/icons/app/splashscreens/ipadpro1_splash.png" media="(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image" />
+                    <link href="/icons/app/splashscreens/ipadpro3_splash.png" media="(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image" />
+                    <link href="/icons/app/splashscreens/ipadpro2_splash.png" media="(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2)" rel="apple-touch-startup-image" />
+                </Head>
                 <body className="dark">
                     <Main />
                     <NextScript />
